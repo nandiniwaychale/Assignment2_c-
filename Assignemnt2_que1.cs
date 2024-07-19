5dbba45064a3c6913d025e057e99702ad748c4f8
Nandini Waychale <nandiniwaychale@gmail.com>
2024-07-19 08:40:01 +0530

    Add files via upload
    This is Assignment 2 of C#

---
+++ Assignemnt2_que1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    internal class Assignemnt2_que1 : IComparable<Assignemnt2_que1>
+    {
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+
+        public Assignemnt2_que1(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public void PrintDetails()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}");
+        }
+        public int CompareTo(Assignemnt2_que1 other)
+        {
+            if (this.Age > other.Age)
+            {
+                return 1;
+            }
+            else if (this.Age < other.Age)
+
+                return -1;
+            else
+                return 0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Assignment 2: 
+            Assignemnt2_que1[] people = new Assignemnt2_que1[3];
+            people[0] = new Assignemnt2_que1("Nandini", 25);
+            people[1] = new Assignemnt2_que1("Tanisha", 20);
+            people[2] = new Assignemnt2_que1("Roshani", 30);
+           
+            Console.WriteLine("People before sorting:");
+            foreach (var person in people)
+            {
+                person.PrintDetails();
+            }
+            Array.Sort(people);
+            Console.WriteLine("\nPeople after sorting:");
+            foreach (var person in people)
+            {
+                person.PrintDetails();
+            }
+        }
+    }
+}
